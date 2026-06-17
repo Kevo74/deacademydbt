7548006a34af3707c53d513e6871f64c9de88f80
Kevin Harris <74kevo@gmail.com>
2026-06-17 16:01:54 +0000

    Lots of changes, may need to revert back to an earlier version. 30 versions in one commit.

---
+++ macros/grant_select.sql
@@ -0,0 +1,8 @@
+{% macro grant_select (schema, role) %}
+        
+        {% set sql %}
+            grant usage on schema {{ schema }} to role {{ role }};
+            grant select on all tables in schema {{ schema }} to role {{ role }};
+            grant select on all views in schema {{ schema }} to role {{ role }};
+        {% endset%}
+{% endmacro %}
